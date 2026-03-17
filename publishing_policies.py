811663f940ec3b022fa6876577473e09bc460396
Ori Shadmon <oshadmon@gmail.com>
2026-03-16 20:09:42 -0700

    tmp
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- publishing_policies.py
+++ publishing_policies.py
@@ -21,13 +21,14 @@ def prep_policy(policy:dict, parent=None):
             del policy["uns"]["dbms"]
         if policy["uns"].get("table"):
             del policy["uns"]["table"]
+    for key in list(policy.get("uns").keys()):
+        if key in ["date", "id", "parent", "ledger"]:
+            del policy["uns"][key]
 
-    if parent:
-        policy["uns"]["parent"] = parent
+    policy["uns"]["parent"] = parent
+    if policy.get("uns").get("uns_level") == "sensor" and policy.get("uns").get("name"):
+        policy["uns"]["name"] = f"{policy.get('uns').get('namespace').rsplit('/', 2)[1]} - {policy["uns"]["name"]}"
 
-    # if policy.get("uns").get("uns_level") == "sensor":
-    #     print(policy)
-    #     exit(1)
     return policy
 
 def check_policy(conn:str="50.116.13.109:32049", policy:dict=None):
@@ -50,7 +51,7 @@ def check_policy(conn:str="50.116.13.109:32049", policy:dict=None):
 
 def publish_policy(conn:str="50.116.13.109:32049", policy:dict=None):
     headers = {
-        "command": "blockchain insert where policy=!new_policy and local=true and master=!ledger_conn",
+        "command": f'blockchain insert where policy=!new_policy and local=true and master=!ledger_conn',
         "User-Agent": "AnyLog/1.23"
     }
 
@@ -65,16 +66,37 @@ if __name__ =="__main__":
     root_policies = {}
     policies = read_file()
     for policy in policies:
-        root_policy = root_policies.get(policy.get("uns").get("namespace").rsplit("/", 1)[0])
+        if policy.get("uns").get("uns_level") == "sensor":
+            # policy["uns"]["name"] = f'{policy.get("uns").get("namespace").rsplit("/", 2)[1]} - {policy["uns"]["name"]}'
+            # print(policy.get("uns").get("namespace"))
+            root_namespace = policy.get("uns").get("namespace").rsplit("/", 1)[0]
+            print(root_namespace)
+            headers = {
+                "command": f'blockchain get uns where [namespace] startswith "Enterprise_C/Site1" and [namespace] endswith "{root_namespace.rsplit("/", 1)[-1]}" bring.first [*][id]',
+                "User-Agent": "AnyLog/1.23"
+            }
+            response = requests.get(url=f"http://50.116.13.109:32049", headers=headers)
+            response.raise_for_status()
+            parent_id = response.text
+            if parent_id == '[]':
+                print(policy)
+                exit(1)
+            policy = prep_policy(policy=policy, parent=parent_id)
 
-        policy_id = check_policy(conn="50.116.13.109:32049", policy=policy)
-        # print(policy_id)
-        if policy_id is None:
-            policy = prep_policy(policy=policy, parent=root_policy)
-        # print(policy)
-        # exit(1)
-            publish_policy(conn="50.116.13.109:32049", policy=policy)
-        policy_id = check_policy(conn="50.116.13.109:32049", policy=policy)
-        if policy.get("uns").get("namespace") and policy.get("uns").get("namespace") not in root_policies:
-            root_policies[policy.get("uns").get("namespace")] = policy_id
+            policy_id = check_policy(conn="50.116.13.109:32049", policy=policy)
+            # # print(policy_id)
+            if policy_id is None:
+                publish_policy(conn="50.116.13.109:32049", policy=policy)
+                # time.sleep(5)
+                # policy_id = check_policy(conn="50.116.13.109:32049", policy=policy)
+
+        #     policy = prep_policy(policy=policy, parent=root_policy)
+        #     # print(policy)
+        #     # if "FIC-250-003/SP_SLPM" in policy.get("uns").get("namespace"):
+        #     #     print(policy.get("uns").get("namespace").rsplit("/", 2)[1] )
+        #     # exit(1)
+        #
+        #     policy_id = check_policy(conn="50.116.13.109:32049", policy=policy)
+        # if policy.get("uns").get("namespace") and policy.get("uns").get("namespace") not in root_policies:
+        #     root_policies[policy.get("uns").get("namespace")] = policy_id
 
